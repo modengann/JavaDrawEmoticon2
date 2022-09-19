7443cae76ec138557391364fac368c685b5aa87a
modengann <99417989+modengann@users.noreply.github.com>
2022-09-18 21:56:09 -0400

    init

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+/*
+** YouTube video: https://www.youtube.com/watch?v=vGpL1BOxHxw&list=PLpkMWkvwtatjEEHLbKGp1HJOggcz2O4sM&index=4
+** Tasks-
+** Basic: Write code that will display the sum of the x and y coordinates whenever the mouse is clicked
+** Advanced: Complete the above AND do one of the follow:
+**           -Go deeper with mouse clicks - maybe create a rectangle wherever you click?
+**           -Explore other input options: keyboard, mousedrag etc
+*/
+
+import javadraw.*;
+public class Main extends Window{
+    
+    public static void main(String[] args) {
+        
+    }
+}
